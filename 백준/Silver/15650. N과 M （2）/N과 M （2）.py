85782d78c597f06b6b84449e283be2368b159e96
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-03-13 23:17:47 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,22 @@
+import sys, time
+read = sys.stdin.readline
+# print(f"{end - start:.5f} sec")
+
+
+########### 백트래킹 풀이 #############
+n,m = list(map(int,input().split()))
+ 
+s = []
+ 
+def dfs(start):
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(start,n+1):
+        if i not in s:
+            s.append(i)
+            dfs(i+1)
+            s.pop()
+ 
+dfs(1)

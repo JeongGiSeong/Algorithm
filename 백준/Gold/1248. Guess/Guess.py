5c227cfa18238019e2a00c09a0ec58db69bc7695
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-03-21 21:45:29 +0900

    [Gold III] Title: Guess, Time: 4324 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1248. Guess/Guess.py
+++ 백준/Gold/1248. Guess/Guess.py
@@ -5,48 +5,44 @@ read = sys.stdin.readline
 # print(f"{end - start:.5f} sec")
 
 
-n = int(input())
-s = input()
-matrix = [[' '] * n for _ in range(n)]
-stack = []
+N = int(input())
+arr = list(input())
 
-cnt = 0
-for i in range(n):
-    for j in range(i, n):
-        matrix[i][j] = s[cnt]
-        cnt += 1
+S = [[0]*N for _ in range(N)]
+
+for i in range(N) :
+    for j in range(i, N) :
+        temp = arr.pop(0)
+        if temp == '+' :
+            S[i][j] = 1
+        elif temp == '-' :
+            S[i][j] = -1
+
+result = [0 for _ in range(N)]
 
 
-def dfs(depth: int):
-    # 가지치기
-    total = 0
-    for i in range(depth - 1, -1, -1):
-        total += stack[i]
-        if matrix[i][depth - 1] == "0":
-            if total != 0:
-                return
-        elif matrix[i][depth - 1] == "-":
-            if total >= 0:
-                return
-        elif matrix[i][depth - 1] == "+":
-            if total <= 0:
-                return
-                
-    if depth == n:
-        print(*stack)
-        # return
-        exit()
-    
-    for i in range(11):
-        if matrix[depth][depth] == '-':
-            stack.append(i * -1)
-        elif matrix[depth][depth] == '0':
-            stack.append(0)
-        else:
-            stack.append(i)
-        
-        dfs(depth + 1)
-        stack.pop()
-    
+def check(idx) :
+    hap = 0
+    for i in range(idx, -1, -1) :
+        hap += result[i]
+        if hap == 0 and S[i][idx] != 0 :
+            return False
+        elif hap < 0 and S[i][idx] >= 0 :
+            return False
+        elif hap > 0 and S[i][idx] <= 0 :
+            return False
+    return True
+
+def dfs(idx) :
+    if idx == N :
+        return True
+
+    # 범위가 -10 ~ 10이면 시간과 가능성 높음
+    for i in range(11) :
+        result[idx] = S[idx][idx] * i
+        if check(idx) and dfs(idx+1) :
+            return True
+    return False
 
 dfs(0)
+print(*result)

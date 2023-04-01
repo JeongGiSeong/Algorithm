a9baaf65583ee8192fd1bc058788d646fa583f6d
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-04-01 20:16:40 +0900

    [Gold III] Title: BFS 스페셜 저지, Time: 516 ms, Memory: 81820 KB -BaekjoonHub

--- 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
+++ 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
@@ -1,41 +1,60 @@
-from collections import deque, defaultdict
+import sys
+from collections import deque as dq
+input = sys.stdin.readline
 
-def bfs():
-    visited = [False] * (N+1)
-    queue = deque()
-    queue.append(1)
-    visited[1] = True
+vertex_num = int(input())
+edge_list = [list(map(int, input().split())) for __ in range(vertex_num-1)]
+solution = list(map(int, input().split()))
 
-    idx = 1
-    while queue:
-        x = queue.popleft()
-        children = []
 
-        for child in GRAPH[x]:
-            if not visited[child]:
-                visited[child] = True
-                children.append(child)
+def make_graph(vertex_num, edge_list):
+    graph = {}
+    for i in range(1, vertex_num+1):
+        graph[i] = []
+    for edge in edge_list:
+        node1, node2 = edge
+        graph[node1].append(node2)
+        graph[node2].append(node1)
 
-        if sorted(answer[idx:idx+len(children)]) == sorted(children):
-            for child in answer[idx:idx+len(children)]:
-                queue.append(child)
-            idx +=len(children)
-        else:
-            return 0
-              
-    return 1
+    return graph
 
-GRAPH = defaultdict(list)
 
-N = int(input())
-for _ in range(N-1):
-    start, end = map(int, input().split())
-    GRAPH[start].append(end)
-    GRAPH[end].append(start)
+def make_ordered_graph(graph, solution):
+    ordered_graph = graph.copy()
+    order = [0] * (vertex_num+1)
 
-answer = list(map(int, input().split()))
+    for i in range(len(solution)):
+        order[solution[i]] = i
+    for i in range(1, len(graph)+1):
+        ordered_graph[i].sort(key=lambda x: order[x])
 
-if answer[0] == 1:
-    print(bfs())
+    return ordered_graph
+
+
+def BFS(graph):
+    que = dq()
+    visited = [0] * (vertex_num+1)
+
+    que.append(1)
+    visited[1] = 1
+    path = [1]
+
+    while que:
+        basev = que.popleft()
+        for nextv in graph[basev]:
+            if visited[nextv] == 0:
+                que.append(nextv)
+                visited[nextv] = 1
+                path.append(nextv)
+
+    return path
+
+
+graph = make_graph(vertex_num, edge_list)
+ordered_graph = make_ordered_graph(graph, solution)
+path = BFS(ordered_graph)
+
+if path == solution:
+    print(1)
 else:
     print(0)

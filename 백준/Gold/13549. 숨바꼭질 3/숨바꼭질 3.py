8eaafd4cf8bb314347bf699764c375af072c0172
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-04-04 15:17:55 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 100 ms, Memory: 36516 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -8,8 +8,6 @@ N, K = map(int, read().split())
 dist = [0] * 100001
 move = [0] * 100001
 
-# DFS로 풀어볼까? -> 가지치기를 엄청 잘하지 않으면 최악의 경우 시간 초과
-# BFS로 풀자
 def bfs(n):
     q = deque()
     q.append(n)
@@ -18,18 +16,13 @@ def bfs(n):
 
         if x == K:
             print(dist[x])
-            # arr = []
-            # temp = x
-            # for _ in range(dist[x]+1):
-            #     arr.append(temp)
-            #     temp = move[temp]
-            # print(' '.join(map(str, arr[::-1])))
             return
 
+        # x*2의 가중치가 0이기 때문에 가장 먼저 큐에 들어가야 함
+        # appendleft를 사용하거나 x*2를 먼저 연산해줘야 함
         for nx in (x*2, x-1, x+1):
             if 0<=nx<=100000 and not dist[nx]:
                 dist[nx] = (dist[x]+1) if nx != x*2 else (dist[x])
                 q.append(nx)
-                # move[nx] = x
 
 bfs(N)

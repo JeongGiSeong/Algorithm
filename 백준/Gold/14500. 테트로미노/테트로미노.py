d6a3b0e24d6da99ac78cbd619987ccbf7ea89409
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-04-05 19:57:10 +0900

    [Gold IV] Title: 테트로미노, Time: 236 ms, Memory: 39076 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+read = sys.stdin.readline
+
+
+def dfs(r, c, idx, total):
+    global ans
+    # 2차원 배열에서 최대값을 찾은 후 남은 블럭 갯수만큼 곱한 수와 비교
+    # - 가장 큰 수를 더해도 ans보다 작다면 가능성이 0이다.
+    if ans >= total + max_val * (3 - idx):
+        return
+    if idx == 3:
+        ans = max(ans, total)
+        return
+    else:
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < N and 0 <= nc < M and visit[nr][nc] == 0:
+                if idx == 1:
+                    visit[nr][nc] = 1
+                    dfs(r, c, idx + 1, total + arr[nr][nc])
+                    visit[nr][nc] = 0
+                visit[nr][nc] = 1
+                dfs(nr, nc, idx + 1, total + arr[nr][nc])
+                visit[nr][nc] = 0
+
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+visit = [([0] * M) for _ in range(N)]
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+ans = 0
+max_val = max(map(max, arr))
+
+for r in range(N):
+    for c in range(M):
+        visit[r][c] = 1
+        dfs(r, c, 0, arr[r][c])
+        visit[r][c] = 0
+
+print(ans)

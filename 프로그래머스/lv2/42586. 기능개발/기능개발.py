930b6033143bbdda00776a2818951f9a4a89aac4
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-04-19 00:00:15 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,21 +1,16 @@
 def solution(progresses, speeds):
     answer = []
-    n = len(progresses)
-    visited = []
-    while True:
-        if sum(answer) == n:
-            return answer
-        
-        for i in range(n):
-            progresses[i] += speeds[i]
-        
-        cnt = 0
-        for i in range(n):
-            if progresses[i] >= 100:
-                if i not in visited:
-                    visited.append(i)
-                    cnt += 1
-            else:
-                break
-        if cnt > 0:
-            answer.append(cnt)
+    time = 0
+    count = 0
+    while len(progresses) > 0:
+        if (progresses[0] + time*speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer

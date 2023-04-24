af6d6f5f8c765090d1299390420128fd91bcd2a3
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-04-24 17:51:39 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -4,12 +4,13 @@ def solution(n, lost, reserve):
     set_reserve = set(reserve) - set(lost)
     
     # 왼쪽부터 확인? 왜?
-    # -> 
-    for i in reversed(list(set_reserve)):
-        if i+1 in set_lost:
-            set_lost.remove(i+1)
-        elif i-1 in set_lost:
+    # -> 앞 원소부터 탐색하기 때문. 오른쪽부터 확인하려면 reversed 사용하면 됨.
+    for i in set_reserve:
+        if i-1 in set_lost:
             set_lost.remove(i-1)
+        # elif로 한 이유 : if로 하면 2명한테 빌려줄 수도 있음
+        elif i+1 in set_lost:
+            set_lost.remove(i+1)
             
     answer = n - len(set_lost)
     return answer

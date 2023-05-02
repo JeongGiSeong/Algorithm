a498d25b0ad457d94c53bc9f179e51ee9a2c0751
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-05-02 14:21:43 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,26 +1,25 @@
-def solution(n, costs):
-    # 비용을 오름차순으로 정렬
-    costs.sort(key=lambda x: x[2])
-    # 연결된 노드 추적
-    connection = [costs[0][0]]
-    # 최종 답안 초기화
+def solution(n: int, costs: list):
+
+    def find_parent(parent: list, n: int) -> int:
+        if parent[n] != n:
+            parent[n] = find_parent(parent, parent[n])
+        return parent[n]
+
+    def union_parent(parent: list, node_a: int, node_b: int):
+        a = find_parent(parent, node_a)
+        b = find_parent(parent, node_b)
+        if a > b:
+            parent[a] = b
+        else:
+            parent[b] = a
+
+    parent = [i for i in range(n)]
     answer = 0
-    # 모든 노드가 연결될 때까지 반복
-    while len(connection) != n:
-        for i, cost in enumerate(costs):
-            # 이미 연결된 경우 건너뛰기
-            if (cost[0] in connection) and (cost[1] in connection):
-                continue
-            # 하나의 노드만 연결된 경우
-            if (cost[0] in connection) or (cost[1] in connection):
-                # 비용 추가
-                answer += cost[2]
-                # 연결된 노드 추가
-                connection.append(cost[0])
-                connection.append(cost[1])
-                # 중복 제거
-                connection = list(set(connection))
-                # 사용한 간선 제거
-                costs[i] = [-1, -1, -1]
-                break
+    costs.sort(key= lambda x:x[2])
+
+    for a, b, cost in costs:
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            answer += cost
+
     return answer

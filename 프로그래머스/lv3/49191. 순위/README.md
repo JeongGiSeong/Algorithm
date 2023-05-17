c3a067b7dffb0cee47699099fbe1e9fb85bf2302
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-05-17 16:11:20 +0900

    [level 3] Title: 순위, Time: 5.76 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/README.md
+++ 프로그래머스/lv3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=python3) 
 
 ### 성능 요약
 
-메모리: 11.2 MB, 시간: 6.18 ms
+메모리: 11.3 MB, 시간: 5.76 ms
 
 ### 구분
 
--- 프로그래머스/lv3/49191. 순위/순위.py
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -1,4 +1,6 @@
 def solution(n, results):
+    answer = 0
+    
     wins = {x: set() for x in range(1, n + 1)}
     loses = {x: set() for x in range(1, n + 1)}
     
@@ -12,4 +14,8 @@ def solution(n, results):
         for loser in wins[player]:
             loses[loser].update(loses[player])
     
-    return len([player for player in range(1, n + 1) if len(wins[player]) + len(loses[player]) == n - 1])
+    for player in range(1, n + 1):
+        if len(wins[player]) + len(loses[player]) == n - 1:
+            answer += 1
+        
+    return answer

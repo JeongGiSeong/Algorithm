b73fb7867833ce5cf67bcea753dabc0781259590
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-09-14 04:01:44 +0900

    [level 3] Title: 합승 택시 요금, Time: 15.57 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -30,6 +30,7 @@ def solution(n, s, a, b, fares):
     for i in range(1, n + 1):
         graph[i] = {}
     for c, d, f in fares:
+        # {c : {d : f}}
         graph[c][d] = f
         graph[d][c] = f
     
@@ -40,12 +41,12 @@ def solution(n, s, a, b, fares):
         # heapq로 우선순위 큐 구현
         heapq.heappush(queue, (0, start))
         while queue:
-            # 큐에서 가장 작은 거리와 해당 정점을 꺼냅니다.
+            # 우선순위 큐 -> 가장 적은 요금과 그 정점이 pop
             current_distance, current_node = heapq.heappop(queue)
-            # 현재 정점까지의 거리가 이미 저장된 거리보다 크다면 무시합니다.
+            # 현재 정점까지의 요금이 이미 저장된 요금보다 크다면 무시
             if distances[current_node] < current_distance:
                 continue
-            # 현재 정점과 인접한 정점들에 대해 반복합니다.
+            # 현재 정점과 인접한 정점들에 대해 반복.
             for adjacent, weight in graph[current_node].items():
                 # 현재 정점까지의 거리와 인접한 정점까지의 요금을 더합니다.
                 distance = current_distance + weight
@@ -53,14 +54,14 @@ def solution(n, s, a, b, fares):
                 if distance < distances[adjacent]:
                     distances[adjacent] = distance
                     heapq.heappush(queue, (distance, adjacent))
-        # 모든 정점까지의 최단 거리를 담은 딕셔너리를 반환합니다.
         return distances
     
-    # 각각 따로 택시를 탔을 때의 비용을 구합니다. 시작점에서 A와 B의 목적지까지의 최단 거리를 구하고 요금을 더합니다.
-    cost = dijkstra(s)[a] + dijkstra(s)[b]
-    # 모든 지점에 대해 합승을 고려해봅니다. 시작점에서 합승 지점까지의 최단 거리와 합승 지점에서 A와 B의 목적지까지의 최단 거리를 구하고 요금을 더합니다.
+    # 각각 따로 택시를 탔을 때의 비용
+    s_result = dijkstra(s)
+    cost = s_result[a] + s_result[b]
+    # 합승 고려
     for i in range(1, n + 1):
-        if s != i: # 시작점과 같은 지점은 제외합니다.
-            cost = min(cost, dijkstra(s)[i] + dijkstra(i)[a] + dijkstra(i)[b]) # 기존 비용과 비교하여 최소값을 갱신합니다.
-    # 최종 비용을 반환합니다.
+        if s != i: # 시작점 제외
+            i_result = dijkstra(i)
+            cost = min(cost, s_result[i] + i_result[a] + i_result[b]) # 기존 비용과 비교하여 최소값을 갱신
     return cost

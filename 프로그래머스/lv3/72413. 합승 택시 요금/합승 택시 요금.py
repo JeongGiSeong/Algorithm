29769ceb4c5e91a0c86cf57492a0fb0d9e801df8
정기성 <80134129+JeongKiSeong@users.noreply.github.com>
2023-09-13 23:45:15 +0900

    [level 3] Title: 합승 택시 요금, Time: 315.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,20 +1,23 @@
+import sys
+
+# 플로이드 와샬
 def solution(n, s, a, b, fares):
-    ans = 200000001
-    cost = [[20000001] * (n+1) for _ in range(n+1)]
-    def floyd_warshall():
-        for k in range(1, n+1):
-            for i in range(1, n+1):
-                for j in range(1, n+1):
-                    if i == j:
-                        cost[i][j] = 0
-                    else:
-                        cost[i][j] = min(cost[i][k] + cost[k][j], cost[i][j])
-        
-    for i, j, c in fares:
-        cost[i][j] = c
-        cost[j][i] = c
-    floyd_warshall()
-        
-    for i in range(1, n+1):
-        ans = min(cost[s][i] + cost[i][a] + cost[i][b], ans)
+    graph = [[sys.maxsize] * (n + 1) for _ in range(n + 1)]
+    for c, d, f in fares: # c와 d 사이 요금이 f
+        graph[c][d] = f
+        graph[d][c] = f
+    
+    # 합승 비용 계산
+    for middle in range(1, n+1):
+        for start in range(1, n+1):
+            for end in range(1, n+1):
+                if start == end:
+                    graph[start][end] = 0
+                graph[start][end] = min(graph[start][end], graph[start][middle] + graph[middle][end])
+    
+    ans = sys.maxsize
+    for t in range(1, n+1):
+        cost = graph[s][t] + graph[t][a] + graph[t][b]
+        ans = min(ans, cost)
+    
     return ans

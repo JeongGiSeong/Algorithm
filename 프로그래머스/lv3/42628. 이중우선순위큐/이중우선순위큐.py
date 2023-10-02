7d51be7cb61ea0775f4cb2b28f4a1e03e7b49f84
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2023-10-02 14:26:36 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,24 @@
+def solution(operations):
+    queue = []
+    
+    for operation in operations:
+        op, num = operation.split()
+        if op == 'I':
+            queue.append(int(num))
+        if op == 'D':
+            # 큐가 비어있으면 삭제 연산 실행 X
+            if len(queue) == 0:
+                continue
+
+            if num == '1':
+                # 최댓값 제거
+                queue.remove(max(queue))
+            else:
+                # 최솟값 제거
+                queue.remove(min(queue))
+                    
+    if len(queue) == 0:
+        return [0, 0]
+    else:
+        return [max(queue), min(queue)]
+                

c2e6109215e99e3c41b262a8d4e16a4297fcce7b
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2023-10-04 00:05:05 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 2.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -17,13 +17,18 @@ def solution(n, t, m, timetable):
     for bus in busTime:
         count = 0 # 버스에 탑승한 크루의 수
         
+        # 조건1: 버스에 자리가 있고,
+        # 조건2: 아직 탑승하지 않은 크루가 남아있으며,
+        # 조건3: 대기 중인 크루가 버스 도착 시간 이전 또는 동시에 도착하면 
         while count < m and idx_crew < len(crewTime) and crewTime[idx_crew] <= bus:
             idx_crew += 1
             count += 1
         
         if count < m: 
+            # 만약 현재 버스에 자리가 남았다면,
             last_person_time = bus
         else:
+            # 만약 현재 버스가 가득 찼다면,
             last_person_time = crewTime[idx_crew - 1] - 1
 
     return minuteToHour(last_person_time)

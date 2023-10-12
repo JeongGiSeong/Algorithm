1d57ca8ca28b20e39d7a4f4ccdbcff0244214f9d
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2023-10-13 01:00:38 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -0,0 +1,18 @@
+def solution(n):
+    # 카탈란 수를 이용해서 푸는 문제
+    # Catalan(n) = 2n! / ( n! * (n+1)! )
+    
+    dp = [0 for i in range(n+1)]
+    dp[0], dp[1] = 1, 1
+
+    for i in range(2, n+1):
+        for j in range(i):
+            dp[i] += dp[j] * dp[i-j-1]
+
+    return dp[n]
+
+#########################################################################################################
+
+    from math import factorial 
+    
+    return factorial(2*n)/(factorial(n+1)*factorial(n))

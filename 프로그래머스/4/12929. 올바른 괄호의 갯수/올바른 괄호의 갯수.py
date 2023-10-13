d6824048c471d3f0c221ca898a24547d2d30d7ae
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2023-10-14 01:07:14 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -2,19 +2,7 @@ def solution(n):
     # 카탈란 수를 이용해서 푸는 문제
     # Catalan(n) = 2n! / ( n! * (n+1)! )
     
-    
-    # 재귀
-    def catalan(n):
-        if n == 0:
-            return 1
-        c = 0
-        for i in range(n):
-            c += catalan(i) * catalan(n-i-1)
-        return c
-    
-    return catalan(n)
-
-    # DP
+    # DP (n이 크면 가장 빠름)
     dp = [0 for i in range(n+1)]
     dp[0] = 1
 
@@ -26,11 +14,22 @@ def solution(n):
 
 #########################################################################################################
 
+    # n이 작으면 가장 빠름
     from math import factorial 
     
     return factorial(2*n)/(factorial(n+1)*factorial(n))
 
 #########################################################################################################
 
+    # 재귀 (중복 부분 문제로 인해 제일 오래 걸림)
+    # catalan(n, c)로 리스트까지 줘서 메모이제이션을 사용하면 빠름
+    def catalan(n, c):
+        if c[n] == 0:
+            for i in range(n):
+                c[n] += catalan(i, c) * catalan(n-i-1, c)
+        return c[n]
+    
+    c = [1] + [0] * n
+    return catalan(n, c)
         
         

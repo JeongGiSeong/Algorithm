0dc846d68cdb38fb6153da793ad612dc9c035235
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2023-10-17 14:30:12 +0900

    [level 3] Title: 야근 지수, Time: 386.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,12 +1,13 @@
 import heapq
-
+    
 def solution(n, works):
     # 최대한 works를 비슷하게 만들기
     
-    if n >= sum(works):
+    if sum(works) <= n:
         return 0
+    
     answer = 0
-    # 기본 값인 최소 힙에서 최대 힙으로 변경
+    # 기본값인 최소 힙에서 최대 힙으로 변환
     works = [-w for w in works]
     heapq.heapify(works)
     while n > 0:
@@ -14,6 +15,17 @@ def solution(n, works):
         heapq.heappush(works, max_val+1)
         n -= 1
 
-    for w in works:
-        answer += w ** 2
-    return answer
+    return sum(list(map(lambda x: x**2, works)))    
+    
+    
+    
+    ########################### 효율성 테스트 실패 (시간 초과) ##########################################
+    if n > sum(works):
+        return 0
+    while n:
+        m = max(works)
+        if m > 0:
+            works[works.index(m)] -= 1
+            n -= 1
+        
+    return sum(list(map(lambda x: x**2, works)))

84f3ea9e034c35b06a9e65cffb7785ea088a875e
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2023-10-20 19:41:02 +0900

    [level 3] Title: 기둥과 보 설치, Time: 42.39 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -0,0 +1,36 @@
+보, 설치 = 1, 1
+기둥, 삭제 = 0, 0
+
+def isImpossible(answer):
+    COL, ROW = 0, 1
+    for x, y, a in answer:
+        if a == 기둥: # 기둥일 때
+            if y != 0 and (x, y-1, 기둥) not in answer and \
+        (x-1, y, 보) not in answer and (x, y, 보) not in answer:
+                return True
+        else: # 보일 때
+            if (x, y-1, 기둥) not in answer and (x+1, y-1, 기둥) not in answer and \
+        not ((x-1, y, 보) in answer and (x+1, y, 보) in answer):
+                return True
+    return False
+
+def solution(n, build_frame):
+    answer = set()
+    
+    for x, y, a, b in build_frame:
+        item = (x, y, a)
+        
+        if b == 설치: # 설치
+            answer.add(item)
+            if isImpossible(answer):
+                answer.remove(item)
+        elif item in answer: # 삭제
+            answer.remove(item)
+            if isImpossible(answer):
+                answer.add(item)
+        # print(answer, sep='\n')
+                
+    answer = list(map(list, answer))
+    answer.sort()
+    
+    return answer

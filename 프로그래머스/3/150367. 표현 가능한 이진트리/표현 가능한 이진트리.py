b55e6eb782f5d1d1329baa675c2e683da01e3045
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2023-12-04 16:16:30 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 31.87 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,29 +1,33 @@
-from math import log2
+# 42 이진수 = 101010
+# 완전이진트리로 바꾸려면 2^h-1
+# 따라서 6 < 2^3-1이므로 h는 3
+# 완전이진트리로 만드려면 앞자리에 그 수만큼 0을 붙이면 됨
+# 0101010. 이제 이게 맞는 완전이진트리인가를 확인해야됨
+# 부모 노드가 0이면 자식들도 무조건 0. 
+import math
 
-
-def is_tree(s, parent):
+def check(b, parent):
+    # 부모가 0이면 자식도 전부 0이여야 됨
     if parent == '0':
-        if not all(child == '0' for child in s):
-            return False
-    if len(s) == 1:
-        return True
-    center = len(s) // 2
-    return is_tree(s[:center], s[center]) and is_tree(s[center + 1:], s[center])
+        if '1' in b:
+            return 0
 
-
-def to_bin(num):
-    s = bin(num)[2:]
-    s_length = int(log2(len(s))) + 1
-    digit = 2 ** s_length - 1
-    return '0' * (digit - len(s)) + s
-
+    if len(b) == 1:
+        return 1
+    center = len(b)//2
+    return check(b[:center], b[center]) and check(b[center+1:], b[center])
 
 def solution(numbers):
     answer = []
+    
     for num in numbers:
-        s = to_bin(num)
-        if is_tree(s, s[len(s) // 2]):
-            answer.append(1)
-        else:
-            answer.append(0)
+        # 2진수 변환
+        num_bin = bin(num)[2:]
+        # 2^n - 1꼴의 자릿수를 가져야함.
+        digit = 2 ** (int(math.log(len(num_bin), 2)) + 1) - 1
+        num_bin = "0" * (digit - len(num_bin)) + num_bin
+        # print(digit, num_bin)
+        
+        answer.append(check(num_bin, num_bin[len(num_bin)//2]))
+    # print(answer)
     return answer

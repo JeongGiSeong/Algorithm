b461053f095e097d86743caf0abbe72330c21f1a
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2023-12-04 16:07:38 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 87.73 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,32 +1,29 @@
-def dfs(b, i, depth):
-    if depth == 0:  	# 리프 노드에 도달했다면
-        return True 	# 포화이진트리
-    
-    # 부모노드가 '0' 일때
-    # 왼쪽 자식 노드가 '1' 이거나 오른쪽 자식 노드가 '1' 이라면 포화 이진트리가 될 수 없음
-    elif b[i] == '0':   
-        if b[i - depth] == '1' or b[i + depth] == '1': return False
+from math import log2
 
-    # 왼쪽 서브 트리 탐색
-    left = dfs(b, i - depth, depth // 2)
-    # 오른쪽 서브 트리 탐색
-    right = dfs(b, i + depth, depth // 2)
-    return left and right
-    
-    
+
+def is_tree(s, parent):
+    if parent == '0':
+        if not all(child == '0' for child in s):
+            return False
+    if len(s) == 1:
+        return True
+    center = len(s) // 2
+    return is_tree(s[:center], s[center]) and is_tree(s[center + 1:], s[center])
+
+
+def to_bin(num):
+    s = bin(num)[2:]
+    s_length = int(log2(len(s))) + 1
+    digit = 2 ** s_length - 1
+    return '0' * (digit - len(s)) + s
+
+
 def solution(numbers):
     answer = []
-    for num in numbers:				# num = 42
-        b = bin(num)[2:]  			# b = 101010 / len(b) = 6
-        nodes = bin(len(b) + 1)[2:] 	# nodes = 7 = 111
-        
-        # 포화이진트리가 아닌 경우 더미노드(0추가)
-        if '1' in nodes[1:]:       
-            dummies = (1 << len(nodes)) - int(nodes, 2)
-            b = '0' * dummies + b
-            
-        # 이미 포화이진트리일 경우
-        result = dfs(b, len(b)//2, (len(b)+1)//4)
-        answer.append(1 if result else 0)
-        
+    for num in numbers:
+        s = to_bin(num)
+        if is_tree(s, s[len(s) // 2]):
+            answer.append(1)
+        else:
+            answer.append(0)
     return answer

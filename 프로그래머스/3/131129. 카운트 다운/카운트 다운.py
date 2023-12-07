5d68f1b1ad20708f23cc879859861dbb4b1e8e18
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2023-12-07 22:54:39 +0900

    [level 3] Title: 카운트 다운, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.py
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.py
@@ -1,41 +1,31 @@
-INF = 987654321
-
-def create_table():
-    arr = []
-    arr.append([i for i in range(1, 21)])
-    arr[0].append(50)
-    nxt = []
-    for i in range(1, 21):
-        for j in range(2, 4):
-            ret = i * j
-            
-            if ret > 20:
-                nxt.append(ret)
-                
-    arr.append(list(set(nxt)))
-    
-    return arr
-
 def solution(target):
-    table = create_table()
-
-    dp = [[INF, 0] for _ in range(target + 1)]
-    dp[0][0] = 0
-
-    for i in range(1, target + 1):
-        for j in range(2):
-            for k in range(len(table[j])):
-                prev = i - table[j][k]
-
-                if prev < 0:
-                    continue
-
-                total, valid = dp[prev][0] + 1, dp[prev][1] + 1 - j
-
-                if total < dp[i][0]:
-                    dp[i] = [total, valid]
-                    
-                elif total == dp[i][0]:
-                    dp[i] = [total, max(dp[i][1], valid)]
-
-    return dp[target]
+    scoreTable = [[0, 0] for _ in range(max(70, target)+1)]
+    for i in range(1, 21):
+        scoreTable[i] = [1, 1]  # 1~20: 싱글
+    for i in range(21, 41):
+        scoreTable[i] = [1, 0]  # 21~40: 더블, 트리플
+    for i in (23, 25, 29, 31, 35, 37):
+        scoreTable[i] = [2, 2]  # 23, 25, 29, 31, 35, 37: 싱글 두 번
+    for i in range(41, 50):
+        scoreTable[i] = [2, 1]  # 41~49: 싱글+더블
+    scoreTable[50] = [1, 1]  # 50: 불 영역
+    for i in range(52, 71):
+        scoreTable[i] = [2, 2]  # 52~70: 싱글 두 번
+    for i in range(42, 61, 3):
+        scoreTable[i] = [1, 0]  # 42~60: 트리플
+    
+    for i in range(71, target+1):
+        triple_20 = i - 60
+        bull = i - 50
+        is_triple_better = False
+        if scoreTable[triple_20][0] < scoreTable[bull][0]:  # 트리플 20이 더 적은 다트를 사용하는 경우
+            is_triple_better = True
+        elif scoreTable[triple_20][0] == scoreTable[bull][0]:  # 다트 수가 같은 경우
+            if scoreTable[bull][1] < scoreTable[triple_20][1]:  # 트리플 20이 더 많은 싱글 또는 불을 맞추는 경우
+                is_triple_better = True
+        if is_triple_better:
+            scoreTable[i] = [scoreTable[triple_20][0] + 1, scoreTable[triple_20][1]]
+        else:
+            scoreTable[i] = [scoreTable[bull][0] + 1, scoreTable[bull][1] + 1]
+    
+    return scoreTable[target]

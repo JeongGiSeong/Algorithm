c1339c10e1f6ddf8bffa4f697dc4a668d642c786
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2024-03-26 11:48:39 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+coins = [int(input()) for _ in range(N)]
+coins.reverse()
+
+ans = 0
+for coin in coins:
+    ans += K // coin
+    K %= coin
+
+print(ans)

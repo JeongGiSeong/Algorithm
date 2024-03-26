fe958ed70b962d858d1f0f2258c94b56307c1b59
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2024-03-26 23:11:50 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,57 +1,32 @@
-import sys, time
-read = sys.stdin.readline
-# start = time.time()
-# end = time.time()
-# print(f"{end - start:.5f} sec")
-
-
-### DFS 풀이 ###
-sys.setrecursionlimit(10**6)
-# 상 하 좌 우
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-
-ans = 0
-def dfs(x, y, cnt):
-    global ans
-
-    if x<0 or x>=n or y<0 or y>=m:
-        return
-
-    if x==(n-1) and y==(m-1):
-        ans = cnt
-        return
-    
-    if maze[x][y] == 1:
-        maze[x][y] = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx, ny, cnt + 1)
-
-### BFS 풀이 ###
 from collections import deque
+
+N, M = map(int, input().split())
+board = [input() for _ in range(N)]
+
+dx = (0, 0, -1, 1)
+dy = (1, -1, 0, 0)
+
+def is_vaild_coord(y, x):
+    return 0 <= y < N and 0 <= x < M
+
 def bfs():
-    queue = deque()
-    # (0, 0)이 시작점
-    queue.append((0, 0))
-    while queue:
-        x, y = queue.popleft()
+    visited = [[False] * M for _ in range(N)]
+    visited[0][0] = True
+    dq = deque()
+    dq.append((0, 0, 1))
+    while dq:
+        y, x, d = dq.popleft()
+
+        if y == N - 1 and x == M - 1:
+            return d
+
         for i in range(4):
-            nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<m and maze[nx][ny]==1:
-                maze[nx][ny] = maze[x][y] + 1
-                queue.append((nx, ny))
+            nx = x + dx[i]
+            if is_vaild_coord(ny, nx) and board[ny][nx] == '1' and not visited[ny][nx]:
+                visited[ny][nx] = True
+                dq.append((ny, nx, d + 1))
 
-        
-# N개의 줄에는 M개의 정수로 미로가 주어진다.
-n, m = map(int, read().split())
-maze = [list(map(int, input())) for _ in range(n)]
+    return -1
 
-# dfs(0, 0, 0)
-# # 마지막 칸 +1
-# print(ans + 1)
-
-bfs()
-print(maze[n-1][m-1])
+print(bfs())

dd0b948e913240fb215af93d84c30762c2b8c2e1
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2024-03-27 17:03:05 +0900

    [Silver II] Title: 이항 계수 2, Time: 304 ms, Memory: 53788 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -5,14 +5,24 @@ MOD = 10007
 N, K = map(int, input().split())
 cache = [[0] * 1001 for _ in range(1001)]
 
-def bino(n, k):
-    if cache[n][k]:
-        return cache[n][k]
-    if k==0 or k==n:
-        cache[n][k] = 1
-    else:
-        cache[n][k] = bino(n-1, k-1) + bino(n-1, k)
-        cache[n][k] %= MOD
-    return cache[n][k]
+# # 탑다운
+# def bino(n, k):
+#     if cache[n][k]:
+#         return cache[n][k]
+#     if k==0 or k==n:
+#         cache[n][k] = 1
+#     else:
+#         cache[n][k] = bino(n-1, k-1) + bino(n-1, k)
+#         cache[n][k] %= MOD
+#     return cache[n][k]
 
-print(bino(N, K))
+# print(bino(N, K))
+
+# 바텀업
+for i in range(1001):
+    cache[i][0] = cache[i][i] = 1
+    for j in range(1, i):
+        cache[i][j] = cache[i-1][j-1] + cache[i-1][j]
+        cache[i][j] %= MOD
+
+print(cache[N][K])

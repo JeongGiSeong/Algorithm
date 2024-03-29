834e9d815ecdba68b7c16dc4638af22e286eb251
정기성 <80134129+JungKiSung1012@users.noreply.github.com>
2024-03-29 16:39:33 +0900

    [Bronze I] Title: 캠핑, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -0,0 +1,9 @@
+cnt = 0
+while True:
+    cnt += 1
+    L, P, V = map(int, input().split())
+    if L == P == V == 0:
+        break
+
+    ans = (V // P) * L + min((V % P), L)
+    print(f'Case {cnt}: {ans}')

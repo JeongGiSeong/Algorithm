99a8bb71b96e6626199fffb308cdd64b0f5d8788
정기성 <80134129+JeongGiSeong@users.noreply.github.com>
2024-07-11 01:15:40 +0900

    [Silver I] Title: 사각 사각, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3359. 사각 사각/사각 사각.py
+++ 백준/Silver/3359. 사각 사각/사각 사각.py
@@ -4,17 +4,15 @@ input = sys.stdin.readline
 # 완전탐색: O(2^n)
 # dp[i][j]: i번째 사각형을 j 방향으로 놨을 때 위쪽의 둘레 최대값
 
-n = int(sys.stdin.readline())
-dp = [[0, 0] for _ in range(n+1)]
-arr = [[0, 0] for _ in range(n+1)]
+n = int(input())
 
-arr[1] = list(map(int, sys.stdin.readline().split()))
-dp[1][0] = arr[1][0]
-dp[1][1] = arr[1][1]
+dp = [[0, 0] for _ in range(n)]
+rect = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(2, n+1):
-    arr[i] = list(map(int, sys.stdin.readline().split()))
-    dp[i][0] = arr[i][0] + max(dp[i-1][0] + abs(arr[i-1][1] - arr[i][1]), dp[i-1][1] + abs(arr[i-1][0] - arr[i][1]))
-    dp[i][1] = arr[i][1] + max(dp[i-1][0] + abs(arr[i-1][1] - arr[i][0]), dp[i-1][1] + abs(arr[i-1][0] - arr[i][0]))
-
-print(max(dp[n]))
+dp[0][0] = rect[0][0]
+dp[0][1] = rect[0][1]
+for i in range(1, n):
+    dp[i][0] = rect[i][0] + max(dp[i-1][0] + abs(rect[i-1][1] - rect[i][1]), dp[i-1][1] + abs(rect[i-1][0] - rect[i][1]))
+    dp[i][1] = rect[i][1] + max(dp[i-1][0] + abs(rect[i-1][1] - rect[i][0]), dp[i-1][1] + abs(rect[i-1][0] - rect[i][0]))
+    
+print(max(dp[n-1]))

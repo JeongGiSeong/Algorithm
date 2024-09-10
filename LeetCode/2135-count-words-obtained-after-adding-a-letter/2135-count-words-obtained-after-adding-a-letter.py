f62874b4c520c011881daf6a57f2eb5ee9deb0de
정기성 <80134129+JeongGiSeong@users.noreply.github.com>
2024-09-11 00:03:18 +0900

    Time: 867 ms (55.96%), Space: 36.5 MB (59.33%) - LeetHub

--- LeetCode/2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
+++ LeetCode/2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
@@ -1,11 +1,22 @@
-# 1. 문자열에 없는 소문자를 끝에 추가
-# 2. 문자열을 임의의 순서로 재배열한다
-# 주어지는 문자열에는 중복되는 문자가 없음
-
-# 1. startword의 모든 문자가 targetword에 있어야 함
-# 2. startword의 길이는 targetword의 길이에 1을 뺀 값
 # startword: [g, j], targetword: [jg] 이걸 1번으로 쳐야된다는건가?
+class Solution:
+    def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
+        startWordSet = set(''.join(sorted(word)) for word in startWords)
+        # targerWordSet을 만들지 않는 이유: 순서가 다른 단어가 정렬되면 같은 단어로 인식되기 때문
+        count = 0
+
+        for targetWord in targetWords:
+            sortedTarget = ''.join(sorted(targetWord))
+            for i in range(len(sortedTarget)):
+                # targetWord에서 1개를 뺀 문자열이 startWordSet에 있는지 확인
+                modifiedTarget = sortedTarget[:i] + sortedTarget[i+1:]
+                if modifiedTarget in startWordSet:
+                    count += 1
+                    break
+
+        return count
         
+####################### 시간 초과 ######################
 # class Solution:
 #     def check(self, startWord: str, targetWord: str)-> bool:
 #         if len(startWord) != len(targetWord)-1:
@@ -27,18 +38,3 @@
 #                     break
 
 #         return ans
-
-class Solution:
-    def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
-        startWordSet = set(''.join(sorted(word)) for word in startWords)
-        count = 0
-
-        for targetWord in targetWords:
-            sortedTarget = ''.join(sorted(targetWord))
-            for i in range(len(sortedTarget)):
-                modifiedTarget = sortedTarget[:i] + sortedTarget[i+1:]
-                if modifiedTarget in startWordSet:
-                    count += 1
-                    break
-
-        return count

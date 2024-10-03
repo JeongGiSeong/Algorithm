628a903ce0fa0201cb65f15e80d1d04853c2d599
정기성 <80134129+JeongGiSeong@users.noreply.github.com>
2024-10-03 18:27:09 +0900

    [Silver II] Title: 황소 다마고치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/23323. 황소 다마고치/황소 다마고치.py
+++ 백준/Silver/23323. 황소 다마고치/황소 다마고치.py
@@ -1,16 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-array = []
-for i in range(41):
-    array.append(2**i)
+array = [2**i for i in range(41)]
+
+def find_max_power(hp):
+    for i in range(41):
+        if hp < array[i]:
+            return i
+    return 41
 
 for _ in range(int(input())):
     hp, food = map(int, input().split())
-
-    tmp = 0
-    for i in range(41):
-        if hp >= array[i]:
-            tmp = i + 1
-
-    print(tmp+food)
+    print(find_max_power(hp) + food)
